7c964253c762b23f1a24ab0297558e9be72dc0bb
stellarmaris <bintang.pmngks@student.ub.ac.id>
2024-08-02 10:35:45 +0700

    testing card 1

--- app/Views/dashboarduser.php
+++ app/Views/dashboarduser.php
@@ -9,8 +9,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <style>
         .container {
-            max-width: 1250px;
+            max-width: 1000px;
             margin-top: 20px;
+        }
+        .custom-btn {
+            width: 377px;
+            height: 179px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 40px;
         }
     </style>
 </head>
@@ -18,5 +26,16 @@
 <body>
     <!-- container -->
     <div class="container">
+         <!-- card test -->
+         <div class="card check-in">
+            <div class="card-body d-flex justify-content-between align-items-center">
+                <a href="#" class="btn btn-primary custom-btn" >Button</a>
+                <div>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
