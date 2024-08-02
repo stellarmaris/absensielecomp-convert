5d02eabdc72a8c362828442ce058825322534982
stellarmaris <bintang.pmngks@student.ub.ac.id>
2024-08-02 13:27:39 +0700

    profile

---
+++ app/Views/profileview.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <style>
+        .container {
+            max-width: 1000px;
+            margin-top: 20px;
+        }
+        .custom-font {
+            font-family: 'Inter', sans-serif; 
+        }
+        body{
+            background-color: #EFF0F7; 
+        }
+        .custom-btn {
+            width: 370px;
+            height: 150px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 40px;
+            font-weight: bold; 
+            background-color:#130C90;
+        }
+        .custom-btn:hover {
+            background-color: #0C074F;
+        }
+        .bold-text {
+            font-weight: 900; 
+        }
+        .card {
+            margin-bottom: 15px;
+            border-radius: 10px;
+            overflow: hidden; 
+        }
+    </style>
+</head>
+<body>
+    <!-- container -->
+    <div class="container">
+        <!-- card title -->
+        <div class="card title-card custom-font">
+            <div class="card-body">
+                <h2 class="card-title bold-text">Profile</h2>
+            </div>
+        </div>
+</body>
+</html>
