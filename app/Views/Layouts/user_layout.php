3719dfdc23bf716f934076ba74d7c578cb866dd9
betgirs <elisabetgirsang@student.ub.ac.id>
2024-08-21 14:06:38 +0700

    bootstrap

--- app/Views/Layouts/user_layout.php
+++ app/Views/Layouts/user_layout.php
@@ -9,6 +9,8 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
     <style>
         body {
             font-family: 'Poppins', sans-serif;
